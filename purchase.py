0ac24da752a2d58c6282bbbf1532e0e79086071b
Kevin Ortega Garcia <kevin.ortega@nan-tic.com>
2024-07-09 10:30:07 +0200

    Purchase update values fix/locale/improved parsing Solved problem where discount formula and the discount parameters where not being updated properly. Now every change should be reflected inmediatly. Created localization for cat and es. Expanded parsing cases of the formula; there where some formats that would produce it to fail, now it should be ironclad. Now for the purchase case, the formula is correctly fetched from the supplier depending on its prices starting and end dates, and if they match with the purchase Task #168262 somewhat

--- purchase.py
+++ purchase.py
@@ -5,14 +5,21 @@ from .discount import DiscountMixin
 class PurchaseLine(DiscountMixin, metaclass=PoolMeta):
     __name__ = 'purchase.line'
 
-    @fields.depends('product', 'discount_formula', 'unit_price', 
-        methods=['apply_discount_formula'])
+    @fields.depends('base_price', 'product', 'discount_formula', 'unit_price',
+        'discount_rate', 'discount_amount', 'purchase_date',
+        methods=['apply_discount_formula', 'on_change_with_discount_rate'])
     def on_change_product(self):
         super().on_change_product()
 
         if self.product and self.product.product_suppliers:
             supplier = self.product.product_suppliers[0]
-            if supplier.prices and supplier.prices[0].discount_formula:
-                self.discount_formula = supplier.prices[0].discount_formula
+            for price in supplier.prices:
+                if(self.purchase_date >= price.start_date
+                    and self.purchase_date <= price.end_date):
+                    self.discount_formula = price.discount_formula
+                    break
+            if self.discount_formula and self.base_price:
                 self.unit_price = self.apply_discount_formula(
                     raise_exception=False)
+                self.discount_rate = self.on_change_with_discount_rate()
+                self.discount_amount = self.on_change_with_discount_amount()
