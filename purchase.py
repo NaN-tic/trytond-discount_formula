d2dc973e85b8414b543d0377174b522b140dd26a
Kevin Ortega Garcia <kevin.ortega@nan-tic.com>
2024-09-10 13:20:59 +0200

    Purch.line amount is updated with discount formula Also, discount formula is updated when product or product supplier is changed.

--- purchase.py
+++ purchase.py
@@ -5,24 +5,35 @@ from .discount import DiscountMixin
 class PurchaseLine(DiscountMixin, metaclass=PoolMeta):
     __name__ = 'purchase.line'
 
+    @fields.depends('amount',methods=['on_change_with_amount'])
+    def on_change_discount_formula(self):
+        super().on_change_discount_formula()
+        self.amount = self.on_change_with_amount()
+
     @fields.depends('base_price', 'product', 'discount_formula', 'unit_price',
-        'discount_rate', 'discount_amount', 'purchase_date',
-        methods=['apply_discount_formula', 'on_change_with_discount_rate'])
+        'discount_rate', 'discount_amount', 'purchase_date', 'product_supplier',
+        methods=['on_change_discount_formula'])
     def on_change_product(self):
         super().on_change_product()
 
-        if self.product and self.product.product_suppliers:
-            supplier = self.product.product_suppliers[0]
-            for price in supplier.prices:
-                if (self.purchase_date
-                        and (not price.start_date
-                             or self.purchase_date >= price.start_date)
-                        and (not price.end_date
-                             or self.purchase_date <= price.end_date)):
-                    self.discount_formula = price.discount_formula
-                    break
-            if self.discount_formula and self.base_price:
-                self.unit_price = self.apply_discount_formula(
-                    raise_exception=False)
-                self.discount_rate = self.on_change_with_discount_rate()
-                self.discount_amount = self.on_change_with_discount_amount()
+        if self.product_supplier and self.product_supplier.prices:
+            self.discount_formula = self.product_supplier.prices[0].discount_formula
+            self.on_change_discount_formula()
+
+
+class PurchaseLineSupplierDepends(DiscountMixin, metaclass=PoolMeta):
+    __name__ = 'purchase.line'
+
+
+    @fields.depends('base_price', 'product', 'discount_formula', 'unit_price',
+        'discount_rate', 'discount_amount', 'purchase_date', 'product_supplier',
+        methods=['on_change_discount_formula'])
+    def on_change_product_supplier(self):
+        try:
+            super().on_change_product_supplier()
+        except AttributeError:
+            pass
+
+        if self.product and self.product_supplier and self.product_supplier.prices:
+            self.discount_formula = self.product_supplier.prices[0].discount_formula
+            self.on_change_discount_formula()
