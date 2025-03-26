a8de861eff906e80eb0e435593585e6c48ea9cb0
nan-tic-dev <79672209+nan-tic-dev@users.noreply.github.com>
2025-03-26 18:12:41 +0100

    Apply discount_formula when show price list (#6)
    #174437

--- price_list.py
+++ price_list.py
@@ -1,6 +1,8 @@
 from trytond.pool import PoolMeta
 from trytond.model import fields
 from trytond.pyson import Bool, Eval
+from trytond.modules.product import round_price
+from .discount import apply_discount_formula
 
 
 class PriceList(metaclass=PoolMeta):
@@ -10,6 +12,21 @@ class PriceList(metaclass=PoolMeta):
         line = self.get_price_line(product, quantity, uom, pattern=pattern)
         if line:
             return line.discount_formula
+
+    def compute(self, product, quantity, uom, pattern=None):
+        unit_price = super().compute(product, quantity, uom, pattern)
+
+        line = self.get_price_line(product, quantity, product.default_uom,
+            pattern=pattern)
+        if line and line.discount_formula is not None:
+            base_price = self.compute_base_price(
+                        product, quantity, product.default_uom, pattern=pattern)
+            if base_price is not None:
+                 base_price = apply_discount_formula(base_price,
+                        line.discount_formula, raise_exception=False)
+            if base_price is not None:
+                unit_price = round_price(base_price)
+        return unit_price
 
 
 class PriceListLine(metaclass=PoolMeta):
@@ -48,14 +65,16 @@ class PriceListLine(metaclass=PoolMeta):
     @fields.depends('discount_formula', 'formula')
     def update_formula(self):
         super().update_formula()
-        if (self.base_price_formula and not self.discount_formula
-                and not self.discount_rate
+        if (self.base_price_formula is not None
+                and not self.discount_formula is None
+                and not self.discount_rate is None
                 and self.formula == '0'):
             self.formula = self.base_price_formula
-        elif self.base_price_formula and self.discount_formula:
+        elif (self.base_price_formula is not None
+                and self.discount_formula is not None):
             self.formula = '0'
 
-    @fields.depends('discount_formula', 'discount_rate', 'formula')
+    @fields.depends(methods=['update_formula'])
     def on_change_discount_formula(self):
         self.discount_rate = None
         self.update_formula()
