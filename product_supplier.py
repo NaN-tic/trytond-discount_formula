97708b6302c5c012ba054ae52c37b661da5b3825
Albert Cervera i Areny <albert@nan-tic.com>
2025-07-18 08:25:32 +0200

    Pyflakes fixes.

--- product_supplier.py
+++ product_supplier.py
@@ -1,6 +1,5 @@
 from trytond.pool import PoolMeta
 from .discount import DiscountMixin
-from trytond.modules.discount_formula.discount import apply_discount_formula
 
 
 class ProductSupplierPrice(DiscountMixin, metaclass=PoolMeta):
